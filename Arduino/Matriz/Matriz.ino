8b381ffd23cbe093e62e13de1e7111371fb0cd37
Estuardo Gabriel Son Mux <82282972+EstuardoSon@users.noreply.github.com>
2022-12-08 12:25:31 -0600

    Documentacion

--- Arduino/Matriz/Matriz.ino
+++ Arduino/Matriz/Matriz.ino
@@ -1,24 +1,25 @@
-int fila[] = {22,23,24,25,26,27,28,29,30,31};
-int column[] = {32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53};
+int fila[] = {22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+int column[] = {32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53};
 int cont = 0;
 unsigned long tiempo;
 String frase;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(13, INPUT); 
-  for(int i = 0; i < 10 ; i++){
-    pinMode(fila[i], OUTPUT);
-    digitalWrite(fila[i], HIGH);
-    
-  }
-  for(int i = 0; i < 22 ; i++){
-    pinMode(column[i], OUTPUT);
-    digitalWrite(column[i], LOW);
-   }
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    pinMode(13, INPUT);
+    for (int i = 0; i < 10; i++)
+    {
+        pinMode(fila[i], OUTPUT);
+        digitalWrite(fila[i], HIGH);
+    }
+    for (int i = 0; i < 22; i++)
+    {
+        pinMode(column[i], OUTPUT);
+        digitalWrite(column[i], LOW);
+    }
 }
-
 
 String generarCadena(String frase)
 {
@@ -225,75 +226,162 @@ String generarCadena(String frase)
         {
             cadena += "000000000";
         }
+        else if (frase[i] == '<')
+        {
+            cadena += "020050088104202";
+        }
+        else if (frase[i] == '>')
+        {
+            cadena += "202104088050020";
+        }
+        else if (frase[i] == '_')
+        {
+            cadena += "001001001001001";
+        }
+        else if (frase[i] == '$')
+        {
+            cadena += "0CC1223FF1120CC";
+        }
+        else if (frase[i] == '¿')
+        {
+            cadena += "00E0112E1001006";
+        }
+        else if (frase[i] == '¡')
+        {
+            cadena += "2FF";
+        }
+        else if (frase[i] == '.')
+        {
+            cadena += "002";
+        }
+        else if (frase[i] == '[')
+        {
+            cadena += "3FF201201201201";
+        }
+        else if (frase[i] == ']')
+        {
+            cadena += "2012012012013FF";
+        }
+        else if (frase[i] == '|')
+        {
+            cadena += "2FF";
+        }
+        else if (frase[i] == ':')
+        {
+            cadena += "022";
+        }
+        else if (frase[i] == ';')
+        {
+            cadena += "001022";
+        }
+        else if (frase[i] == ',')
+        {
+            cadena += "001002000";
+        }
+        else if (frase[i] == '~')
+        {
+            cadena += "002004002002004";
+        }
+        else if (frase[i] == '\\')
+        {
+            cadena += "3000C003000C003";
+        }
+        else if (frase[i] == '\'')
+        {
+            cadena += "1C0";
+        }
+        else if (frase[i] == '"')
+        {
+            cadena += "0001C00001C0000";
+        }
+        else if (frase[i] == '@')
+        {
+            cadena += "1FF1111291FD001";
+        }
+        else if (frase[i] == '¡')
+        {
+            cadena += "2FF";
+        }
+        else if (frase[i] == '}')
+        {
+            cadena += "2012011EE010000";
+        }
+        else if (frase[i] == '{')
+        {
+            cadena += "0000101EE201201";
+        }
         else
         {
-            cadena += "0EC1122391120EC"; //sorpresa :v
+            cadena += "0EC1122391120EC"; // sorpresa :v
         }
         cadena += "000000";
     }
     return cadena;
 }
 
-String generarBinario(String hexdec){
-  String cadena ="";
-  int i = 0;
- 
-    while (hexdec[i]) {
- 
-        switch (hexdec[i]) {
+String generarBinario(String hexdec)
+{
+    String cadena = "";
+    int i = 0;
+
+    while (hexdec[i])
+    {
+
+        switch (hexdec[i])
+        {
         case '0':
-            cadena+="0000";
+            cadena += "0000";
             break;
         case '1':
-            cadena+="0001";
+            cadena += "0001";
             break;
         case '2':
-            cadena+="0010";
+            cadena += "0010";
             break;
         case '3':
-            cadena+="0011";
+            cadena += "0011";
             break;
         case '4':
-            cadena+="0100";
+            cadena += "0100";
             break;
         case '5':
-            cadena+="0101";
+            cadena += "0101";
             break;
         case '6':
-            cadena+="0110";
+            cadena += "0110";
             break;
         case '7':
-            cadena+="0111";
+            cadena += "0111";
             break;
         case '8':
-            cadena+="1000";
+            cadena += "1000";
             break;
         case '9':
-            cadena+="1001";
+            cadena += "1001";
             break;
         case 'A':
         case 'a':
-            cadena+="1010";
+            cadena += "1010";
             break;
         case 'B':
         case 'b':
-            cadena+="1011";
+            cadena += "1011";
             break;
         case 'C':
         case 'c':
-            cadena+="1100";
+            cadena += "1100";
             break;
         case 'D':
         case 'd':
-            cadena+="1101";
+            cadena += "1101";
             break;
         case 'E':
         case 'e':
-            cadena+="1110";
+            cadena += "1110";
             break;
         case 'F':
         case 'f':
-            cadena+="1111";
+            cadena += "1111";
             break;
         default:
             break;
@@ -301,39 +389,44 @@ String generarBinario(String hexdec){
         i++;
     }
     return cadena;
-  }
+}
 
-
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  while(Serial.available() == 0){}
-  frase = Serial.readString();
-  frase.toUpperCase();
-  String cadena = generarCadena(frase);
-
-  while (digitalRead(13) == HIGH){
-  for (int c = 21; c >= 0 ; c--){
-    digitalWrite(column[c],HIGH);
-    String hexa = "";
-    hexa+=cadena[cont%cadena.length()];
-    hexa+=cadena[(cont+1)%cadena.length()];
-    hexa+=cadena[(cont+2)%cadena.length()];
-    String letraBinario = generarBinario(hexa); 
-    letraBinario = letraBinario.substring(2, letraBinario.length());
-    for (int f = 0; f < 10 ; f++){
-        if(letraBinario[f] == '1'){
-          digitalWrite(fila[f],LOW);
-          //tiempo = millis();
-          //while(millis() < 1+tiempo){}
-          delay(1);
-          digitalWrite(fila[f],HIGH);
-       }
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    while (Serial.available() == 0)
+    {
     }
-    cont = cont + 3;
-    digitalWrite(column[c],LOW);
-  }
-  cont = cont - 63;
-  }
-  cont = 0;
+    frase = Serial.readString();
+    frase.toUpperCase();
+    String cadena = generarCadena(frase);
+
+    while (digitalRead(13) == HIGH)
+    {
+        for (int c = 21; c >= 0; c--)
+        {
+            digitalWrite(column[c], HIGH);
+            String hexa = "";
+            hexa += cadena[cont % cadena.length()];
+            hexa += cadena[(cont + 1) % cadena.length()];
+            hexa += cadena[(cont + 2) % cadena.length()];
+            String letraBinario = generarBinario(hexa);
+            letraBinario = letraBinario.substring(2, letraBinario.length());
+            for (int f = 0; f < 10; f++)
+            {
+                if (letraBinario[f] == '1')
+                {
+                    digitalWrite(fila[f], LOW);
+                    // tiempo = millis();
+                    // while(millis() < 1+tiempo){}
+                    delay(1);
+                    digitalWrite(fila[f], HIGH);
+                }
+            }
+            cont = cont + 3;
+            digitalWrite(column[c], LOW);
+        }
+        cont = cont - 63;
+    }
+    cont = 0;
 }
